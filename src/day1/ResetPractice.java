22a937d80ef8e4755897b1eb3bdac7ee499a0b67
Ahmet YILMAZ <53831139+ahmetYilmaz88@users.noreply.github.com>
2021-07-26 09:52:46 -0700

    The case is all done

--- src/day1/ResetPractice.java
+++ src/day1/ResetPractice.java
@@ -4,4 +4,7 @@ public class ResetPractice {
     //part 1 is done
     //part 2 is done
     //part 3 is done
+    //part 4 is done
+    //part 5 is done
+    //Mixed reset remove from histrory but keep the file. part 4 and 5 committed together
 }
