4ab53a80a41c975e1da45ab152ad7cc50eb3a73b
Ahmet YILMAZ <53831139+ahmetYilmaz88@users.noreply.github.com>
2021-07-29 12:52:47 -0700

    watching 10 more minutes

--- src/day3/Movie.java
+++ src/day3/Movie.java
@@ -1,9 +1,5 @@
 package day3;
 
 public class Movie {
-    //watching 10 more minutes
-    //watching 20 more minutes
-    //watching 30 more minutes
-    //watching 40 more minutes
-
+    //Watch until 10 minutes
 }
